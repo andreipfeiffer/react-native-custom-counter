a56c63caf1ab82fc2bf2fd9e70dbead2514cbf43
Andrei Pfeiffer <andrei@upsidedown.ro>
2018-03-29 15:18:55 +0200

    Implement React counter

--- App.js
+++ App.js
@@ -4,35 +4,33 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  TouchableOpacity
+} from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  state = {
+    count: 1
+  };
 
-type Props = {};
-export default class App extends Component<Props> {
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <TouchableOpacity
+          style={[styles.wrapper, styles.border]}
+          onPress={this.increment}
+        >
+          <Text style={styles.button}>{this.state.count}</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -41,18 +39,19 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: "stretch"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  border: {
+    borderColor: "#eee",
+    borderBottomWidth: 1
   },
+  button: {
+    fontSize: 50,
+    color: "orange"
+  }
 });
